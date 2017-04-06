252b40ca055911af6fc4a5c9a51ec242837d2b20
Josh Steele <josh@steelethis.com>
2017-04-06 17:27:22 -0400

    Set up OWIN pipeline with ASP.NET web API.

---
+++ DemoAPI/Model/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DemoAPI.Model
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public string Phone { get; set; }
+    }
+}
---
+++ DemoAPI/Startup.cs
@@ -0,0 +1,20 @@
+﻿using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace DemoAPI
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+            config.MapHttpAttributeRoutes();
+
+            app.UseWebApi(config);
+        }
+    }
+}
